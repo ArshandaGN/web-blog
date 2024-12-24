255eaf594990c5f4c4b6b93564ea867f71bceed4
arshaa06 <arshandagn06@gmail.com>
2024-12-25 00:23:35 +0700

    nambah

--- resources/views/components/front/home-page.blade.php
+++ resources/views/components/front/home-page.blade.php
@@ -1,18 +1,32 @@
     <x-front.layout>
+        <!-- Page Header -->
+        <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
+            <div class="container position-relative px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <div class="site-heading">
+                            <h1>ARBlog</h1>
+                            <span class="subheading">Simpel blog untuk berbagi informasi</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
         <!-- Main Content-->
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
                     <!-- Post preview-->
                     <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
+                        <a href="{{ url('post') }}">
+                            <h2 class="post-title" >Man must explore, and this is exploration at its greatest</h2>
                             <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
                         </a>
                         <p class="post-meta">
                             Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 24, 2023
+                            <a href="#!">Arshan</a>
+                            on September 24, 2024
                         </p>
                     </div>
                     <!-- Divider-->
@@ -22,8 +36,8 @@
                         <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
                         <p class="post-meta">
                             Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 18, 2023
+                            <a href="#!">Arshan</a>
+                            on September 18, 2024
                         </p>
                     </div>
                     <!-- Divider-->
@@ -36,8 +50,8 @@
                         </a>
                         <p class="post-meta">
                             Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on August 24, 2023
+                            <a href="#!">Arshan</a>
+                            on August 24, 2024
                         </p>
                     </div>
                     <!-- Divider-->
@@ -50,8 +64,8 @@
                         </a>
                         <p class="post-meta">
                             Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on July 8, 2023
+                            <a href="#!">Arshan</a>
+                            on July 8, 2024
                         </p>
                     </div>
                     <!-- Divider-->
--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,41 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Member\BlogController;
 
+// Static Pages Routes
 Route::get('/', function () {
     return view('components.front.home-page');
-});
+})->name('home');
 
+Route::get('/about', function () {
+    return view('components.front.about-page');
+})->name('about');
+
+Route::get('/post', function () {
+    return view('components.front.post-page');
+})->name('post');
+
+Route::get('/contact', function () {
+    return view('components.front.contact-page');
+})->name('contact');
+
+// Dashboard Route
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Authenticated Routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    //blog route
-    Route::get('/member/blogs',[BlogController::class,'index']);
-
-    Route::get('/member/blogs/{post}/edit',[BlogController::class,'edit']);
-
+    // Blog Routes
+    Route::get('/member/blogs', [BlogController::class, 'index']);
+    Route::get('/member/blogs/{post}/edit', [BlogController::class, 'edit']);
     Route::resource('members/blogs', BlogController::class)->names([
-        'index'=> 'member.blogs.index',
-        'edit'=> 'member.blogs.edit',
+        'index' => 'member.blogs.index',
+        'edit' => 'member.blogs.edit',
     ]);
 });
 
 require __DIR__.'/auth.php';
-
